6f0e37a4a40375fa230d2df3f1bb1ff49d686540
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-09 19:58:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -121,7 +121,7 @@ def adding_to_dennis
         }
      }
 
-  programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+  programmer_hash[:dennis_ritchie][:languages] << 
   programmer_hash
 
 end
